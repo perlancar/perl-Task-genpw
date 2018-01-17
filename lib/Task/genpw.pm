891e8f6e1ead1a31ecec5cfdff111bd3bba9b278
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-17 09:44:11 +0700

    Initial commit

---
+++ lib/Task/genpw.pm
@@ -0,0 +1,21 @@
+package Task::genpw;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Install the various genpw-* programs
+
+=pkggroup Included modules
+
+=pkg App::genpw
+
+=pkg App::genpw::base56
+
+=pkg App::genpw::base58
+
+=pkg App::genpw::id
+
+=pkg App::genpw::wordlist
+
+=cut
